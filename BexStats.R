366f0172fe52486b1a946d24113dc020a3649eca
Makipurple <michaeldu203@gmail.com>
2024-11-24 23:36:17 +0100

    Export of DyadSUmmary

---
+++ BexStats.R
@@ -0,0 +1,30 @@
+
+
+
+# Import DSI ELO and Bex For Maerge
+
+
+
+
+BexDSI <- read.csv("/Users/maki/Desktop/Master Thesis/BEX 2223 Master Thesis Maung Kyaw/IVPToleranceBex2223/BexDSI.csv")
+
+BexElo <- read.csv("/Users/maki/Desktop/Master Thesis/BEX 2223 Master Thesis Maung Kyaw/IVPToleranceBex2223/BexElo.csv")
+
+DyadSummary <- read.csv("/Users/maki/Desktop/Master Thesis/BEX 2223 Master Thesis Maung Kyaw/IVPToleranceBex2223/dyad_summary_clean.csv")
+
+
+print(BexDSI)
+print(BexElo)
+print(DyadSummary)
+
+
+
+
+
+#Create Final Bex for statistics:
+  
+  #  "Date", "Month", "DyadDay", "Trial", "Time", "Period", "Male", "Female", "Dyad",
+  #"DyadDistance", "Proximity", "DyadResponse", "SpecialBehaviour", "Context", "ToleranceBinomial",
+  # MAge", "FAge", "AgeDiff", "IEloDiff", "Tol", "Agg", "NotApp"
+  
+  
